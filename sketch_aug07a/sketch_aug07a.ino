07f3f461ed19d7ff5bd3c794660382d14609b23c
nickscode1 <42130181+nickscode1@users.noreply.github.com>
2018-08-07 13:12:21 -0700

    Add files via upload

---
+++ sketch_aug07a/sketch_aug07a.ino
@@ -0,0 +1,20 @@
+int opendelay = 500;
+int closedelay = 1000;
+void setup() {
+  // put your setup code here, to run once:
+  for (int i = 2; i < 4; i++) {
+    pinMode(i, OUTPUT);
+  }//setup loop
+}//setup
+
+void loop() {
+  for (int i = 2; i < 4; i++) {
+    digitalWrite (i, LOW);
+    delay (opendelay);
+    digitalWrite (i, HIGH);
+    delay (closedelay);
+  }//pattern loop
+ }//void loop
+  // put your main code here, to run repeatedly:
+
+
